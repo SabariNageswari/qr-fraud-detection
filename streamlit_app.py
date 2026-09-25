901ee624b255e57864d7ab383c483c735c8e84a8
SabariNageswari <188465150+SabariNageswari@users.noreply.github.com>
2026-09-25 21:23:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,30 @@
+import subprocess
+import time
+import streamlit as st
 
+
+@st.cache_resource
+def start_flask_backend():
+    process = subprocess.Popen(
+        [
+            "gunicorn",
+            "--bind",
+            "127.0.0.1:5000",
+            "app.main:app"
+        ]
+    )
+
+    time.sleep(5)
+
+    return process
+
+
+# Start Flask backend
+start_flask_backend()
+
+
+# Start your existing Streamlit UI
+exec(
+    open("app/streamlit_ui.py", encoding="utf-8").read(),
+    globals()
+)
